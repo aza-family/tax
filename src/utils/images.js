a66f202302a82f4220b14cc0821cdf9084596239
negabaro <negabaro@naver.com>
2021-10-04 23:55:29 +0900

    fix image(dynamic import)

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,64 +1,11 @@
-import zero from "@/assets/0.png";
-import one from "@/assets/1.png";
-import two from "@/assets/2.png";
-import there from "@/assets/3.png";
-import four from "@/assets/4.png";
-import five from "@/assets/5.png";
-import six from "@/assets/6.png";
-import seven from "@/assets/7.png";
-import eight from "@/assets/8.png";
-import nine from "@/assets/9.png";
+function importAll(r) {
+  return r.keys().map(r);
+}
 
-export default [
-  {
-    value: 0,
-    src: zero,
-    name: "0000",
-  },
-  {
-    value: 1,
-    src: one,
-    name: "1111",
-  },
-  {
-    value: 2,
-    src: two,
-    name: "2222",
-  },
-  {
-    value: 3,
-    src: there,
-    name: "3333",
-  },
-  {
-    value: 4,
-    src: four,
-    name: "4444",
-  },
-  {
-    value: 5,
-    src: five,
-    name: "5555",
-  },
-  {
-    value: 6,
-    src: six,
-    name: "6666",
-  },
-  {
-    value: 7,
-    src: seven,
-    name: "77777",
-  },
+const images = importAll(
+  require.context("@/assets/", false, /\.(png|jpe?g|svg|webp)$/)
+);
 
-  {
-    value: 8,
-    src: eight,
-    name: "8888",
-  },
-  {
-    value: 9,
-    src: nine,
-    name: "9999",
-  },
-];
+export default images.map(function (image, index) {
+  return { value: index, src: image };
+});
